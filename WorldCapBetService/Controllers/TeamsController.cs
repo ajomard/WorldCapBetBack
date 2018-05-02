aa8407a493da82ac35c98ea31b91b92170f2a4ea
Arnaud Kubler <arnaud.kubler@gmail.com>
2018-05-02 15:19:40 +0200

    try to fix token

--- WorldCapBetService/Controllers/TeamsController.cs
+++ WorldCapBetService/Controllers/TeamsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using WorldCapBetService.Models.Entities;
 
 namespace WorldCapBetService.Controllers
 {
+    [Authorize(Policy = "ApiUser")]
     [Produces("application/json")]
     [Route("api/Teams")]
     public class TeamsController : Controller
