0512f3f88a348f485243a7ae76f16bd54c07d236
Arnaud Kubler <arnaud.kubler@gmail.com>
2018-05-20 21:16:46 +0200

    create chartscontroller

---
+++ WorldCapBetService/Controllers/ChartsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WorldCapBetService.Data;
+
+namespace WorldCapBetService.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Charts")]
+    public class ChartsController : Controller
+    {
+        private readonly Context _context;
+        private readonly ChartsManager _chartsManager;
+
+        public ChartsController(Context context)
+        {
+            _context = context;
+            _chartsManager = new ChartsManager(context);
+        }
+
+        // GET: api/ChartsAvererage/5
+        [Authorize(Policy = "ApiUser")]
+        [HttpGet("Average/{userId}")]
+        public ActionResult GetChartsAvererage([FromRoute] string userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var chart = _chartsManager.GetAverageStatsFromUser(userId);
+
+
+            if (chart == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(chart);
+        }
+    }
+}
---
+++ WorldCapBetService/Data/ChartsManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCapBetService.Models.Entities;
+using WorldCapBetService.ViewModels;
+
+namespace WorldCapBetService.Data
+{
+    public class ChartsManager
+    {
+        private readonly Context _context;
+
+        public ChartsManager(Context context)
+        {
+            _context = context;
+        }
+
+        public ChartAverageStats GetAverageStatsFromUser(string userId)
+        {
+            var user = _context.User.SingleOrDefault(u => u.Id == userId);
+            if (user != null)
+            {
+                //chartAverageStats.UserStats = _context.Rankings.SingleOrDefault(r => r.User.Id == userId);
+                var allRankings = _context.Rankings.ToList();
+                var averageGoodPronosticAndGoodScore = allRankings.Average(r => r.GoodPronosticAndGoodScore);
+                var averageGoodGoalAverage = _context.Rankings.Average(r => r.GoodGoalAverage);
+                var averageGoodPronosticOnly = _context.Rankings.Average(r => r.GoodPronosticOnly);
+                var averageFalsePronostic = _context.Rankings.Average(r => r.FalsePronostic);
+                var averageScore = _context.Rankings.Average(r => r.Score);
+
+                var chartAverageStats = new ChartAverageStats();
+                var userStats = _context.Rankings.SingleOrDefault(r => r.User.Id == userId);
+
+                chartAverageStats.BarList = new List<GroupingBar>();
+                //chartAverageStats.BarList.Add(CreateAverageStatBar(userStats.Score, averageScore, "Score", user));
+                chartAverageStats.BarList.Add(CreateAverageStatBar(userStats.GoodPronosticAndGoodScore, averageGoodPronosticAndGoodScore, "Good Pronostic And Good Score", user));
+                chartAverageStats.BarList.Add(CreateAverageStatBar(userStats.GoodGoalAverage, averageGoodGoalAverage, "Good Goal Average", user));
+                chartAverageStats.BarList.Add(CreateAverageStatBar(userStats.GoodPronosticOnly, averageGoodPronosticOnly, "Good Pronostic Only", user));
+                chartAverageStats.BarList.Add(CreateAverageStatBar(userStats.FalsePronostic, averageFalsePronostic, "False Pronostic", user));
+
+                return chartAverageStats;
+            }
+            return null;
+        }
+
+        private GroupingBar CreateAverageStatBar(int userStat, double averageStat, string groupingName, User user)
+        {
+            var userName = user.FirstName + " " + user.LastName;
+
+            var groupingBar = new GroupingBar();
+            groupingBar.Name = groupingName;
+            groupingBar.Series = new List<Bar>();
+            var userBar = new Bar(userName, userStat);
+            var averageBar = new Bar("Average", averageStat);
+
+            groupingBar.Series.Add(userBar);
+            groupingBar.Series.Add(averageBar);
+
+            return groupingBar;
+        }
+
+    }
+}
